9f783908fe3dfc9b1052837d06ebcb398dd040a1
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-08 11:23:43 +0300

    add firebase, redux-thunk and fetch posts from firebase

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,15 +2,16 @@ import _ from "lodash";
 
 import { FETCH_POSTS, UPDATE_POST, DELETE_POST, CREATE_POST } from "./types";
 
-const INITAL_POSTS = {
-  123: "One wierd trick",
-  456: "Bet you wanted this"
-};
+import { Posts } from "../firebase";
 
 export function fetchPosts() {
-  return {
-    type: FETCH_POSTS,
-    payload: INITAL_POSTS
+  return dispatch => {
+    Posts.on("value", snapshot => {
+      dispatch({
+        type: FETCH_POSTS,
+        payload: snapshot.val()
+      });
+    });
   };
 }
 
